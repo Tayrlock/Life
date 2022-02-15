02eb0c1a7b2a2bd7f502353f467c4d3801571ad0
Tayrlock <dkfear@ya.ru>
2022-02-15 21:05:32 +0300

    first work, fix field, no grid

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const btnStart = document.querySelector('#start')
+const btnStart = document.querySelector('#start'),
+    btnApply = document.querySelector('#apply'),
+    btnStop = document.querySelector('#stop'),
+    btnReset = document.querySelector('#reset')
+
+function startLife(play){
 
 
-btnStart.addEventListener('click', (e) => {
-            gameField()
-    
+}
+// Start button
+btnStart.addEventListener('click', function startLife() {
+    life();
+    // if (play = true) { btnStart.removeEventListener('click', startLife) }
+})
+
+// Apply field size
+btnApply.addEventListener('click', () => {
+    createField()
+    gameField()
+})
+
+// Stop button
+btnStop.addEventListener('click', () => {
+    clearTimeout(step)
+    // play = false
+    // btnStart.addEventListener('click', startLife())
+})
+
+
+// reset button
+btnReset.addEventListener('click', () => {
+    createField()
+    gameField()
+    clearTimeout(step)
 })
--- life.js
+++ life.js
@@ -1,41 +1,90 @@
 const canvas = document.getElementById('life'),
-    ctx = canvas.getContext('2d')
-    
-    
+    ctx = canvas.getContext('2d');
+let cell = [],
+    step
 
-
-    console.log(canvas)
-    
-    
-
-// x.textContent = '10123'
-
-// координаты
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
-}
-canvas.addEventListener('mousemove', function (evt) {
-    var mousePos = getMousePos(canvas, evt);
-    let x = document.getElementById('input_x')
-    let y = document.getElementById('input_y')
-    x.textContent = mousePos.x
-    y.textContent = mousePos.y
-})
-
-// Игровое поле
-function gameField(){
+// Игровое поле формирование
+function createField() {
     let cellSize = document.querySelector('#cell').value || 10
-    console.log(cellSize)
-    canvas.width = document.querySelector('#width').value || 1000
-    canvas.height = document.querySelector('#height').value || 1000
+    // console.log(cellSize)
+    canvas.width = document.querySelector('#width').value || 300
+    canvas.height = document.querySelector('#height').value || 300
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    
 }
 
+// Игровое поле заполнение массивами
+
+function gameField() {
+    for (let i = 0; i < 30; i++) {
+        cell[i] = []
+        for (let j = 0; j < 30; j++) {
+            cell[i][j] = 0
+        }
+    }
+}
+
+// Получение клеток по координатам
+
+canvas.onclick = function (e) {
+    let x = e.offsetX
+    let y = e.offsetY
+    console.log(x, y)
+    x = Math.floor(x / 10)
+    y = Math.floor(y / 10)
+    console.log(y, x)
+    cell[y][x] = 1
+    console.log(cell)
+    drawCell()
+}
+
+function drawCell() {
+    // console.log(cell)
+    ctx.clearRect(0, 0, 300, 300)
+    for (let i = 0; i < 30; i++) {
+        for (let j = 0; j < 30; j++) {
+            if (cell[i][j] == 1) {
+                ctx.fillRect(j * 10, i * 10, 10, 10)
+            }
+        }
+    }
+}
+
+function life() {
+    let play = true
+    let newCell = []
+    for (let i = 0; i < 30; i++) {
+        newCell[i] = []
+        for (let j = 0; j < 30; j++) {
+            let live = 0
+            if (cell[torEnd(i) + 1][j] == 1) live++ //[1,0] down ↓
+            if (cell[torBgn(i) - 1][j] == 1) live++ //[-1,0] up ↑
+            if (cell[i][torEnd(j) + 1] == 1) live++ //[0,1] right →
+            if (cell[i][torBgn(j) - 1] == 1) live++ //[0,-1]  left ←
+            if (cell[torEnd(i) + 1][torEnd(j) + 1] == 1) live++ //[1,1] ↓→
+            if (cell[torBgn(i) - 1][torEnd(j) + 1] == 1) live++ //[-1,1] ↑→
+            if (cell[torBgn(i) - 1][torBgn(j) - 1] == 1) live++ //[-1,-1] ←↑
+            if (cell[torEnd(i) + 1][torBgn(j) - 1] == 1) live++ //[1,-1] ←↓
+            if (live == 2) { newCell[i][j] = cell[i][j] } // 2 live = stay, 3 live = born, other = dead
+            else {
+                if (live == 3) { newCell[i][j] = 1 }
+                else { newCell[i][j] = 0 }
+            }
+        }
+    }
+    // console.log(newCell)
+    cell = newCell
+    drawCell()
+    step = setTimeout(life, 2)
+}
+
+function torBgn(i) {
+    if (i == 0) return 30
+    else return i
+}
+function torEnd(i) {
+    if (i == 29) return -1
+    else return i
+}
 // gameField()
 // var n = y * canvas.width + x;
 // console.log(n)
@@ -76,4 +125,19 @@ function gameField(){
 // context.moveTo(0,5.5);
 // context.lineto(5,5.5);
 
+// координаты
+// function getMousePos(canvas, evt) {
+//     var rect = canvas.getBoundingClientRect();
+//     return {
+//         x: evt.clientX - rect.left,
+//         y: evt.clientY - rect.top
+//     };
+// }
+// canvas.addEventListener('mousemove', function (evt) {
+//     var mousePos = getMousePos(canvas, evt);
+//     let x = document.getElementById('input_x')
+//     let y = document.getElementById('input_y')
+//     x.textContent = mousePos.x
+//     y.textContent = mousePos.y
+// })
 
